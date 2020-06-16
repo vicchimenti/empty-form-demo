6168152f4fc39ae7b48d874e8fbc64f9f38f54ab
Vic Chimenti <chimenti@seattleu.edu>
2020-06-16 12:06:12 -0700

    rm cm code endpoint routing disables

--- Startup.cs
+++ Startup.cs
@@ -17,9 +17,8 @@ namespace empty_form_demo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-/*            MvcOptions.EnableEndpointRouting = false;
-*/        
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,16 +31,6 @@ namespace empty_form_demo
 
             app.UseMvc();
 
-/*            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-*/
         }
     }
 }

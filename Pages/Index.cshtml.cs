19efdca835c958331438656a468f8a92771955fb
Vic Chimenti <chimenti@seattleu.edu>
2020-06-16 12:55:31 -0700

    add tag helpers

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 
 namespace empty_form_demo.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; set; }
+
+
         public void OnGet()
         {
+            Message = "Enter your message";
+        }
 
+        public void OnPost()
+        {
+            Message = Request.Form[nameof(Message)];
         }
     }
 }
